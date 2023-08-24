a78fb4b523ff3c5aecdaa42252224ccc4641b26c
Амир <amir.66garaev@gmail.com>
2023-08-24 21:39:38 +0300

    pass

--- route/main/admin.py
+++ route/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile,Route
+from .models import Profile,Route,GasStation
 
 
 admin.site.register(Profile)
 admin.site.register(Route)
+admin.site.register(GasStation)
--- route/main/models.py
+++ route/main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
+
 class GasStation(models.Model):
     region = models.CharField(max_length=100)
     Issuer_number = models.CharField(max_length=200)
@@ -9,7 +10,6 @@ class GasStation(models.Model):
     Address = models.CharField(max_length=200)
     GPS_coordinates_latitude = models.FloatField()
     GPS_coordinates_longitude = models.FloatField()
-    Companion_Service_Objects = models.CharField(max_length=200)
 
     def __str__(self):
         return f"Gas Station {self.Gas_station_number} - {self.Address}"
@@ -28,10 +28,7 @@ class Route(models.Model):
     name = models.CharField(max_length=200)
     start_point = models.CharField(max_length=200)
     end_point = models.CharField(max_length=200)
-    total_weight = models.DecimalField(max_digits=10, decimal_places=2)
-    vehicle_height = models.DecimalField(max_digits=10, decimal_places=2)
-    axle_load = models.DecimalField(max_digits=10, decimal_places=2)
-    #RefuelingOnTheRoute = models.CharField(max_length=200)
+    RefuelingOnTheRoute = models.TextField(default='')
 
     def __str__(self):
         return self.name
